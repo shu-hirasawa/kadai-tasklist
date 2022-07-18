f1aedafd0256d32b00bd0e7197c3419959105c80
shu.hirasawa <shu.hirasawa@outlook.jp>
2022-07-18 21:27:24 +0900

    コントローラ7種ひな形コピー DB関連ファイル追加

---
+++ src/controllers/NewServlet.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import java.io.IOException;
+// import java.sql.Timestamp;
+
+// import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Message;
+// import utils.DBUtil;
+
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public NewServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // CSRF対策
+        request.setAttribute("_token", request.getSession().getId());
+
+        // おまじないとしてのインスタンスを生成
+        request.setAttribute("message", new Message());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+        rd.forward(request, response);
+     }
+}
